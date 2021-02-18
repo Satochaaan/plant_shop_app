2dfa0b36d3ba4dd155350d065ecd447aa963f25a
Satochaaan <k97satosi327@gmail.com>
2021-02-19 08:46:25 +0900

    ヘッダーテキスト調整

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class _PlantShopHomePageState extends State<PlantShopHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: kColorGreen,
         leading: Icon(Icons.arrow_back_ios_rounded),
         actions: <Widget>[
@@ -58,11 +59,9 @@ class _PlantShopHomePageState extends State<PlantShopHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
+            _Header(),
           ],
         ),
       ),
@@ -95,6 +94,121 @@ class _PlantShopHomePageState extends State<PlantShopHomePage> {
         currentIndex: _selectedIndex,
         type: BottomNavigationBarType.fixed,
         onTap: _onPageChanged,
+      ),
+    );
+  }
+}
+
+class _HeaderCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    return Path()
+      ..lineTo(0, size.height * 0.6)
+      ..quadraticBezierTo(
+        size.width * 0.5,
+        size.height,
+        size.width,
+        size.height * 0.6,
+      )
+      ..lineTo(size.width, 0)
+      ..close();
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class _Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double height = 400;
+    return Container(
+      height: height,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: _HeaderBackground(height: height),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Our Plants',
+                    style: TextStyle(fontSize: 32.0, color: kColorWhite),
+                  ),
+                  Text(
+                    'Lorem Ipsum Dammy Plants',
+                    style: TextStyle(fontSize: 16.0, color: kColorWhite),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Price:\$66',
+                        style: TextStyle(fontSize: 16.0, color: kColorWhite),
+                      ),
+                      SizedBox(
+                        width: 30,
+                        child: Text(
+                          "|",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: kColorWhite),
+                        ),
+                      ),
+                      Text(
+                        'Size: Medium',
+                        style: TextStyle(fontSize: 16.0, color: kColorWhite),
+                      ),
+                      SizedBox(
+                        width: 30,
+                        child: Text(
+                          "|",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: kColorWhite),
+                        ),
+                      ),
+                      Text(
+                        'Plant: Gimam',
+                        style: TextStyle(fontSize: 16.0, color: kColorWhite),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _HeaderBackground extends StatelessWidget {
+  final double height;
+
+  const _HeaderBackground({
+    Key key,
+    @required this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: _HeaderCurveClipper(),
+      child: Container(
+        width: double.infinity,
+        height: height,
+        decoration: BoxDecoration(
+          color: kColorGreen,
+        ),
       ),
     );
   }
